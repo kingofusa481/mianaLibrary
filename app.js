686e5e095fea4e2d2475bf7a5eff48527a084026
ahmadumar312 <kingofusa481@gmail.com>
2025-02-03 20:17:11 +0500

    path updated

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const ejs = require("ejs")
 
 const app = express();
 const port = 3000;
+const path = require("path");
 
 app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
 
 app.get('/', (req, res) => {
     res.render("index");
