cc7268de3d0d3c8d2314c0a21df2c6f288b675b6
kingofusa481 <56435413+kingofusa481@users.noreply.github.com>
2025-02-04 19:11:19 +0500

    error solving

--- db/connectDB.js
+++ db/connectDB.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+        const conn = await mongoose.connect("mongodb+srv://ahmadumar312:dbmianapass@cluster0.ozjgl.mongodb.net/miana-library?retryWrites=true&w=majority&appName=Cluster0", {
             serverSelectionTimeoutMS: 5000,
             socketTimeoutMS: 45000,
         });
